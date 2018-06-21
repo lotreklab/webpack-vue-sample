c91663a680f738308c7480537bba508e3863bd26
Andrea Stagi <stagi.andrea@gmail.com>
2018-06-21 11:43:02 +0200

    Split in two components

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,14 @@
 <template>
   <div class="full-width center-content">
-    {{ msg }}
+    <hello-component name="World" />
   </div>
 </template>
 
 <script>
-  export default {
-    data () {
-      return {
-        msg: 'Hello world!'
-      }
-    }
+import HelloComponent from './components/HelloComponent.vue'
+export default {
+  components: {
+    HelloComponent
   }
+}
 </script>
-
-<style scoped>
-  .full-width {
-    width: 100%;
-  }
-  .center-content {
-    display: flex;
-    justify-content: center;
-    align-items: center;
-  }
-</style>
---
+++ src/components/HelloComponent.vue
@@ -0,0 +1,26 @@
+<template>
+  <div class="full-width center-content">
+    {{ msg }}
+  </div>
+</template>
+
+<script>
+  export default {
+    data () {
+      return {
+        msg: 'Hello world!'
+      }
+    }
+  }
+</script>
+
+<style scoped>
+  .full-width {
+    width: 100%;
+  }
+  .center-content {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+  }
+</style>
